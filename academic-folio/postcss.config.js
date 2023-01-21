483029de625e34d7d7faae8e8a2c5fcf88b0d1d2
Rohan Deb Sarkar <rohandebsarkar@protonmail.com>
2023-01-21 09:44:00 +0530

    Update postcss

--- academic-folio/postcss.config.js
+++ academic-folio/postcss.config.js
@@ -1,11 +1,6 @@
-module.exports = {    
-    plugins: [   
-        require('postcss-import')({
-            path: ['.']
-            }), 
-        require('tailwindcss')('tailwind.config.js'),
-        require('autoprefixer')({
-            path: ['.']
-        }),
-    ]
+module.exports = {
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    }
 }
