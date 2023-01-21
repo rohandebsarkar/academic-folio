d8d2a002b851f67c1d88e99920b7f927912dac28
Rohan Deb Sarkar <rohandebsarkar@protonmail.com>
2023-01-21 14:30:21 +0530

    fix tailwind css

--- academic-folio/tailwind.config.js
+++ academic-folio/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./content/**/*.{html,js}", "./layouts/**/*.{html,js}"],
+  content: ["./content/**/*.{md,html}", "./layouts/**/*.{html,js}"],
   theme: {
     extend: {},
   },
