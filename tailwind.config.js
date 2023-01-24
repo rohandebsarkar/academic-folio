cd4d18dd923af2c63e569517de1ee06bcac411dc
Rohan Deb Sarkar <rohandebsarkar@protonmail.com>
2023-01-24 21:55:28 +0530

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,22 @@ module.exports = {
   darkMode: "class",
   content: ["./content/**/*.{md,html}", "./layouts/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: '#f2f2f2',
+          800: '#0f0f0f',
+        },
+        secondary: {
+          100: '#828282',
+          800: '#828282',
+        },
+        theme: {
+          100: '#0000d0',
+          800: '#00f1ff',
+        }
+      }
+    },
   },
   plugins: [
     require("nightwind"),
