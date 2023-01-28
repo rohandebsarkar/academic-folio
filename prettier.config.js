441ee8e2e6131a2fb3569ade1d40de5d02404217
Rohan Deb Sarkar <rohandebsarkar@protonmail.com>
2023-01-29 01:31:09 +0530

    update prettier configuaration

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,14 @@
 module.exports = {
-  trailingComma: "es5",
-  tabWidth: 4,
-  semi: true,
-  singleQuote: true,
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    overrides: [
+        {
+            files: ['*.html'],
+            options: {
+                parser: 'go-template',
+            },
+        },
+    ],
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class",
-  content: ["./content/**/*.{md,html}", "./layouts/**/*.{html,js}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: "var(--global-text-color-primary)",
-        secondary: "var(--global-text-color-shade)",
-        background: "var(--global-bg-color)",
-        theme: "var(--global-theme-color)",
-        header: "var(--global-header-color)",
-        footer: "var(--global-footer-color)",
-        divider: "var(--global-divider-color)",
-      },
+    darkMode: 'class',
+    content: ['./content/**/*.{md,html}', './layouts/**/*.{html,js}'],
+    theme: {
+        extend: {
+            colors: {
+                primary: 'var(--global-text-color-primary)',
+                secondary: 'var(--global-text-color-shade)',
+                background: 'var(--global-bg-color)',
+                theme: 'var(--global-theme-color)',
+                header: 'var(--global-header-color)',
+                footer: 'var(--global-footer-color)',
+                divider: 'var(--global-divider-color)',
+            },
+        },
     },
-  },
 };
