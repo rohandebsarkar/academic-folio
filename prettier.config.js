6069c3cecca533f49bc793589c7ab1c64b1309e7
Amir Pourmand <pourmand1376@gmail.com>
2023-02-10 16:07:28 +0330

    add changes

--- prettier.config.js
+++ prettier.config.js
@@ -3,4 +3,12 @@ module.exports = {
     tabWidth: 4,
     semi: true,
     singleQuote: true,
+    overrides: [
+        {
+            files: ['*.html'],
+            options: {
+                parser: 'go-template',
+            },
+        },
+    ],
 };
