587e86f45f4fabc444152620474c7384db4b927d
Rohan Deb Sarkar <rohandebsarkar@protonmail.com>
2023-02-20 10:47:25 +0530

    fix build

--- exampleSite/tailwind.config.js
+++ exampleSite/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     darkMode: 'class',
-    content: ['./content/**/*.{md,html}', './themes/**/layouts/**/*.{html,js}'],
+    content: ['./content/**/*.{md,html}', './themes/academic-folio/layouts/**/*.{html,js}'],
     theme: {
         extend: {
             colors: {
