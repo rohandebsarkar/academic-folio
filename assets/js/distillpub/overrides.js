2a650e0639b0207501847d09fcb4a04535e51691
Rohan Deb Sarkar <rohandebsarkar@protonmail.com>
2024-08-21 08:31:00 +0530

    fix distill

--- assets/js/distillpub/overrides.js
+++ assets/js/distillpub/overrides.js
@@ -1,56 +1,58 @@
 document.addEventListener('readystatechange', () => {
-    // Override styles of the footnotes.
-    document.querySelectorAll('d-footnote').forEach(function (footnote) {
-        footnote.shadowRoot
-            .querySelector('sup > span')
-            .setAttribute('style', 'color: var(--global-theme-color);');
-        footnote.shadowRoot
-            .querySelector('d-hover-box')
-            .shadowRoot.querySelector('style')
-            .sheet.insertRule(
-                '.panel {background-color: var(--global-bg-color) !important;}'
-            );
-        footnote.shadowRoot
-            .querySelector('d-hover-box')
-            .shadowRoot.querySelector('style')
-            .sheet.insertRule(
-                '.panel {border-color: var(--global-divider-color) !important;}'
-            );
-    });
-    // Override styles of the citations.
-    document.querySelectorAll('d-cite').forEach(function (cite) {
-        cite.shadowRoot
-            .querySelector('div > span')
-            .setAttribute('style', 'color: var(--global-theme-color);');
-        cite.shadowRoot
-            .querySelector('style')
-            .sheet.insertRule(
-                'ul li a {color: var(--global-text-color-primary) !important; text-decoration: none;}'
-            );
-        cite.shadowRoot
-            .querySelector('style')
-            .sheet.insertRule(
-                'ul li a:hover {color: var(--global-theme-color) !important;}'
-            );
-        cite.shadowRoot
-            .querySelector('d-hover-box')
-            .shadowRoot.querySelector('style')
-            .sheet.insertRule(
-                '.panel {background-color: var(--global-bg-color) !important;}'
-            );
-        cite.shadowRoot
-            .querySelector('d-hover-box')
-            .shadowRoot.querySelector('style')
-            .sheet.insertRule(
-                '.panel {border-color: var(--global-divider-color) !important;}'
-            );
-    });
-    // Override styles of the math.
-    document.querySelectorAll('d-math').forEach(function (math) {
-        math.shadowRoot
-            .querySelector('style')
-            .sheet.insertRule(
-                'span .katex {color: var(--global-text-color-primary) !important;}'
-            );
-    });
+    if (document.readyState === 'complete') {
+        // Override styles of the footnotes.
+        document.querySelectorAll('d-footnote').forEach(function (footnote) {
+            footnote.shadowRoot
+                .querySelector('sup > span')
+                .setAttribute('style', 'color: var(--global-theme-color);');
+            footnote.shadowRoot
+                .querySelector('d-hover-box')
+                .shadowRoot.querySelector('style')
+                .sheet.insertRule(
+                    '.panel {background-color: var(--global-bg-color) !important;}'
+                );
+            footnote.shadowRoot
+                .querySelector('d-hover-box')
+                .shadowRoot.querySelector('style')
+                .sheet.insertRule(
+                    '.panel {border-color: var(--global-divider-color) !important;}'
+                );
+        });
+        // Override styles of the citations.
+        document.querySelectorAll('d-cite').forEach(function (cite) {
+            cite.shadowRoot
+                .querySelector('div > span')
+                .setAttribute('style', 'color: var(--global-theme-color);');
+            cite.shadowRoot
+                .querySelector('style')
+                .sheet.insertRule(
+                    'ul li a {color: var(--global-text-color-primary) !important; text-decoration: none;}'
+                );
+            cite.shadowRoot
+                .querySelector('style')
+                .sheet.insertRule(
+                    'ul li a:hover {color: var(--global-theme-color) !important;}'
+                );
+            cite.shadowRoot
+                .querySelector('d-hover-box')
+                .shadowRoot.querySelector('style')
+                .sheet.insertRule(
+                    '.panel {background-color: var(--global-bg-color) !important;}'
+                );
+            cite.shadowRoot
+                .querySelector('d-hover-box')
+                .shadowRoot.querySelector('style')
+                .sheet.insertRule(
+                    '.panel {border-color: var(--global-divider-color) !important;}'
+                );
+        });
+        // Override styles of the math.
+        document.querySelectorAll('d-math').forEach(function (math) {
+            math.shadowRoot
+                .querySelector('style')
+                .sheet.insertRule(
+                    'span .katex {color: var(--global-text-color-primary) !important;}'
+                );
+        });
+    }
 });
